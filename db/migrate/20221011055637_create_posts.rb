d1bc733285983718d5f959f69c0b711eb681638c
EC2 Default User <ec2-user@ip-172-31-24-28.ap-northeast-1.compute.internal>
2022-10-11 06:25:25 +0000

    [add]モデル・テーブルの作成

---
+++ db/migrate/20221011055637_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :posts do |t|
+
+      t.integer :customer_id
+      t.string :title
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20221011055815_create_reviews.rb
@@ -0,0 +1,13 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+
+      t.integer :customer_id
+      t.integer :item_id
+      t.string :evaluation
+      t.text :text
+
+      t.timestamps
+    end
+  end
+end
