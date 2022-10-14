8f4ce2f3c21838e1887f3a64d1f9ad4a505fe072
hitoshi9616 <108915808+hitoshi9616@users.noreply.github.com>
2022-10-14 06:24:45 +0000

    [add]ItemsControllerのcreateアクションを修正・genre機能製作中

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -1,2 +1,37 @@
 class Admin::GenresController < ApplicationController
+
+  def index
+    @genres = Genre.all
+    @genre = Genre.new
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+  end
+
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to admin_genre_path(@genre.id)
+    else
+      render :edit
+    end
+  end
+
+  def create
+    genre = Genre.new(genre_params)
+    if genre.save
+      redirect_to admin_genre_path(genre.id)
+    else
+      @genres = Genre.all
+      render index
+  end
+
+  def destroy
+  end
+
 end
--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -5,8 +5,8 @@ class Admin::ItemsController < ApplicationController
   end
 
   def create
-    item = Item.new(item_params)
-    if item.save
+    @item = Item.new(item_params)
+    if @item.save
       redirect_to admin_item_path(item.id)
     else
       render :new
