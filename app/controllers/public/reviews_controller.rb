9dcc5873552b31fee2e4d1935af53d0d7e6a6930
hitoshi9616 <108915808+hitoshi9616@users.noreply.github.com>
2022-10-15 08:49:25 +0000

    [add]customer側で商品を見られるようにした。review機能作成途

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -1,2 +1,41 @@
 class Public::ReviewsController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_customer.id
+    #@review.item_id = item.id
+    if @review.save
+      redirect_to review_path(@review.id)
+    else
+      render item_path(item.id)
+    end
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+    if review.update(review_params)
+      redirect_to review_path(@review.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to reviews_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:evaluation, :text)
 end
