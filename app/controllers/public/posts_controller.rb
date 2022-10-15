c1c8a8615e8a89c10c4b2d063edd67bf516caaf3
hitoshi9616 <108915808+hitoshi9616@users.noreply.github.com>
2022-10-15 10:08:33 +0000

    [add]public側のpostコントローラーを記述・離脱のためcommit

--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -1,2 +1,48 @@
 class Public::PostsController < ApplicationController
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.customer_id = current_customer.id
+    if @post.save
+      redirect_to post_path(@post.id)
+    else
+      render :new
+    end
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to post_path(@post.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to :index
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text, :genre_id)
 end
