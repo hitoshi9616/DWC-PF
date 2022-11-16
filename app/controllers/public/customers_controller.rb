71e768389d2fce87898860351a32fb7c1da04c66
hitoshi9616 <108915808+hitoshi9616@users.noreply.github.com>
2022-11-16 07:06:09 +0000

    [add]public側の会員情報編集でparamsの中に変更データを入れられるように修正

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -48,7 +48,7 @@ class Public::CustomersController < ApplicationController
   private
 
   def customer_params
-    params.require(:customer).permit(:nickname)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :phone_number, :age, :email)
   end
 
 end
